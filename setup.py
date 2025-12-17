4a8e0c6f441bb940139bdd0fd2a714e907134120
mglhbr <miguel.huber@bluewin.ch>
2025-12-17 12:03:57 +0100

    updated env and setup.py for proper installation

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="orca_core",
+    version="0.0.0",
+    description="ORBIT Teleop ORCA core utilities and drivers.",
+    packages=find_packages(),
+    install_requires=[],
+    include_package_data=True,
+)
